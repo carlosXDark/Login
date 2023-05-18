52ec7c7221989941250450e42570c0f3065432b0
carlosXDark <128709018+carlosXDark@users.noreply.github.com>
2023-05-18 02:20:37 -0300

    Update index.tsx

--- src/pages/CompletedLogin/index.tsx
+++ src/pages/CompletedLogin/index.tsx
@@ -1,7 +1,7 @@
 export function CompletedLogin() {
   return (
   <div className="container" style={{ backgroundImage: "url('https://gifs.eco.br/wp-content/uploads/2022/06/gifs-de-anime-lofi-9.gif')" }}>
-  <h1 style={{ fontSize: '50px', color: "pink", marginBottom: '700px'}}>PARABÉNS VOCÊ FEZ O LOGIN COM SUSSESSO.</h1>
+  <h1 style={{ fontSize: '50px', color: "pink", marginBottom: '700px'}}>PARABÉNS VOCÊ FEZ O LOGIN COM SUCESSO.</h1>
   </div>
   );
   }
